b58f8f1b3e6346806b6dec2b936eacf1aae2ecf2
= <=>
2025-04-06 06:50:42 +0300

    Third update

--- practice_db.sql
+++ practice_db.sql
@@ -5,12 +5,12 @@ CREATE TABLE Patient(
     PatientID VARCHAR(10) NOT NULL PRIMARY KEY,
     Name VARCHAR(30) NOT NULL,
     Age INT NOT NULL,
-    Gender CHAR(1),
+    Gender VARCHAR(8),
     Contact VARCHAR(10),
     CONSTRAINT chk_contact CHECK (LENGTH(Contact) = 10),
-    CONSTRAINT chk_name CHECK (BINARY name = UPPER(name)),
-    CONSTRAINT chk_age CHECK (Age < 3),
-    CONSTRAINT chk_gender CHECK (Gender = 'M' OR Gender = 'F')
+    CONSTRAINT chk_Patientname CHECK (BINARY name = UPPER(name)),
+    CONSTRAINT chk_age CHECK (Age > 3),
+    CONSTRAINT chk_patientgender CHECK (Gender = 'Male' OR Gender = 'Female')
 );
 
 CREATE TABLE Doctor(
@@ -31,6 +31,35 @@ CREATE TABLE Appointment(
     CONSTRAINT fhk_doctor FOREIGN KEY (DoctorID) REFERENCES Doctor(DoctorID)
 );
 
+CREATE TABLE Prescription(
+    PrescriptionID VARCHAR(5) NOT NULL PRIMARY KEY,
+    AppointmentID VARCHAR(10) NOT NULL,
+    Medicine VARCHAR(20),
+    Dosage VARCHAR(10),
+    CONSTRAINT fhk_appointment FOREIGN KEY (AppointmentID) REFERENCES Appointment(AppointmentID)
+);
+
+ALTER TABLE Patient 
+DROP CONSTRAINT chk_age;
+
+ALTER TABLE Patient
+ADD CONSTRAINT chk_age CHECK (Age > 3);
+
+
+INSERT INTO Patient VALUES('P001','JOHN KINTU',35,'M','0700111111');
+INSERT INTO Patient VALUES('P002','SARAH NAMBI',28,'F','0700222222');
+INSERT INTO Patient VALUES('P003','PAUL OKELLO',42,'M','0700333333');
+
+ALTER TABLE Doctor
+MODIFY COLUMN Contact VARCHAR(10);
+
+INSERT INTO Doctor VALUES('101','Alex Bukenya','BPharm', '0700444444');
+INSERT INTO Doctor VALUES('102','Diana Musoke','BPharm', '0700555555');
+INSERT INTO Doctor VALUES('103','Solomon Opio','DPharm', '0700666666');
+
+INSERT INTO Appointment VALUES('101','Alex Bukenya','BPharm', 0700444444);
+INSERT INTO Appointment VALUES('101','Alex Bukenya','BPharm', 0700444444);
+INSERT INTO Appointment VALUES('101','Alex Bukenya','BPharm', 0700444444);
 
 UPDATE activity
 SET name = 'Jim Jones'
@@ -38,3 +67,5 @@ WHERE ActivityCode = Ao1
 
 
 CREATE VIEW view1
+AS SELECT * FROM Patient
+WHERE Gender = 'M';
